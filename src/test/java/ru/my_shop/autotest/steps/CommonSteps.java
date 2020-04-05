db835485852f42e19b20b032e5faebf8b0f88032
fkost <f.kostyuchenko@solarl.ru>
2020-04-05 18:42:24 +0300

    Описание шагов в классе CommonSteps

--- src/test/java/ru/my_shop/autotest/steps/CommonSteps.java
+++ src/test/java/ru/my_shop/autotest/steps/CommonSteps.java
@@ -1,19 +1,100 @@
 package ru.my_shop.autotest.steps;
 
-import cucumber.api.java.en.Given;
-import cucumber.api.java.en.When;
+import cucumber.api.java.ru.И;
+import cucumber.api.java.ru.Когда;
+import cucumber.api.java.ru.То;
+import cucumber.api.java.ru.Тогда;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
-public class CommonSteps {
+public class CommonSteps extends AbstractSteps{
 
+    private static final Logger logger
+            = LoggerFactory.getLogger(CommonSteps.class);
 
-    @Given("^Проверка - Шаг 1$")
-    public void checkStepOne() {
-        System.out.println("Проверка: Шаг 1");
+    @Когда("^выполняет поиск товара \"([^\"]*)\"$")
+    public void searchProduct(String productName) {
+
     }
 
-    // todo вроде можно указвать {} вместо "([^"]*)", но не факт.
-    @When("^Проверка - Шаг 2 с параметром \"([^\"]*)\"$")
-    public void checkStepOneWithParameter(String parameter) {
-        System.out.printf("Проверка: Шаг 1  параметром %s%n", parameter);
+    @То("^проверяет результаты поиска по ключевому слову \"([^\"]*)\"$")
+    public void checkResultsByKeyword(String productName) {
+        logger.info("Example log from {}", "asa");
+    }
+
+    @И("^выводит информацию о первых (\\d+)-ти товарах$")
+    public void printInfoAboutProducts(int amountProducts ) {
+
+    }
+
+    @Тогда("^сортирует \"([^\"]*)\"$")
+    public void sort(String typeSorting) {
+        logger.debug("Example log from {}", "asa");
+    }
+
+    @И("^проверяет сортировку \"([^\"]*)\"$")
+    public void checkSorting(String typeSorting) {
+
+    }
+
+    @Когда("^открывает первый товар в списке$")
+    public void openFirstProductInList() {
+
+    }
+
+    @Когда("^выводит информацию о товаре$")
+    public void printProductInfo() {
+
+    }
+
+    @И("^выбирает раздел \"([^\"]*)\"$")
+    public void selectSection(String sectionName) {
+
+    }
+
+    @Когда("^нажимает элемент меню \"([^\"]*)\"$")
+    public void clickElementMenu(String elementName) {
+
+    }
+
+    @И("^выбирает категорию \"([^\"]*)\"$")
+    public void selectCategory(String categoryName) {
+
+    }
+
+    @И("^выбирает подкатегорию \"([^\"]*)\"$")
+    public void selectSubcategory(String subcategoryName) {
+
+    }
+
+    @И("^выбирает подраздел \"([^\"]*)\"$")
+    public void selectSubsection(String subsectionName) {
+
+    }
+
+    @И("^устанавливает количество товара \"([^\"]*)\" шт в блоке 'Купить'$")
+    public void setAmountOfProductInBuyBlock(String amount) {
+
+    }
+
+    @И("^добавляет товар в корзину из каточки товара$")
+    public void addProductToCartFromProductCard () {
+
+    }
+
+    @И("^нажимает кнопку \"([^\"]*)\"$")
+    public void clickButton(String buttonName) {
+
+    }
+
+
+    @И("^устанавливает параметр \"([^\"]*)\"$")
+    public void setParameter(String parameterName) {
+
+    }
+
+    @И("^проверяет наличие и количество товара \"([^\"]*)\" шт в корзине$")
+    public void checksAvailabilityAndAmountOfProductInCart(String amountProduct) {
+
     }
 }
