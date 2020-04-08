f041ee75f7e769e693a8b0c7cb332d3bf1b21c09
fkost <f.kostyuchenko@solarl.ru>
2020-04-09 01:03:39 +0300

    Добавлены шаги в CommonSteps

--- src/test/java/ru/my_shop/autotest/steps/CommonSteps.java
+++ src/test/java/ru/my_shop/autotest/steps/CommonSteps.java
@@ -4,42 +4,46 @@ import cucumber.api.java.ru.И;
 import cucumber.api.java.ru.Когда;
 import cucumber.api.java.ru.То;
 import cucumber.api.java.ru.Тогда;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import ru.my_shop.autotest.pages.*;
+
 
 public class CommonSteps extends AbstractSteps{
 
-    private static final Logger logger
-            = LoggerFactory.getLogger(CommonSteps.class);
+    private CommonPage commonPage = new CommonPage();
+    private CardProductPage cardProductPage = new CardProductPage();
+    private CatalogPage catalogPage = new CatalogPage();
+    private HomePage homePage = new HomePage();
 
     @Когда("^выполняет поиск товара \"([^\"]*)\"$")
     public void searchProduct(String productName) {
-
+        // todo добавить логи для каждого шага
+        commonPage.searchProduct(productName);
     }
 
     @То("^проверяет результаты поиска по ключевому слову \"([^\"]*)\"$")
     public void checkResultsByKeyword(String productName) {
-        logger.info("Example log from {}", "asa");
+        catalogPage.checkSearchResults(productName);
     }
 
-    @И("^выводит информацию о первых (\\d+)-ти товарах$")
+    @И("^выводит информацию о первых - (\\d+) товарах$")
     public void printInfoAboutProducts(int amountProducts ) {
-
+        // todo раскоментить, после испольлзования модели.
+//        catalogPage.printSearchResults(amountProducts);
     }
 
-    @Тогда("^сортирует \"([^\"]*)\"$")
-    public void sort(String typeSorting) {
-        logger.debug("Example log from {}", "asa");
+    @Тогда("^сортирует товары \"([^\"]*)\"$")
+    public void sortProduct(String typeSorting) {
+        catalogPage.sortProduct(typeSorting);
     }
 
     @И("^проверяет сортировку \"([^\"]*)\"$")
     public void checkSorting(String typeSorting) {
-
+        catalogPage.checkSorting(typeSorting);
     }
 
-    @Когда("^открывает первый товар в списке$")
-    public void openFirstProductInList() {
-
+    @И("^открыть товар по номеру (\\d+) в списке$")
+    public void openProductInList(int numberProduct) {
+        catalogPage.openProductByNumberInList(numberProduct);
     }
 
     @Когда("^выводит информацию о товаре$")
@@ -49,42 +53,42 @@ public class CommonSteps extends AbstractSteps{
 
     @И("^выбирает раздел \"([^\"]*)\"$")
     public void selectSection(String sectionName) {
-
+        catalogPage.selectCatalogElement(sectionName);
     }
 
     @Когда("^нажимает элемент меню \"([^\"]*)\"$")
     public void clickElementMenu(String elementName) {
-
+        commonPage.clickElementMenu(elementName);
     }
 
     @И("^выбирает категорию \"([^\"]*)\"$")
     public void selectCategory(String categoryName) {
-
+        commonPage.selectCategory(categoryName);
     }
 
     @И("^выбирает подкатегорию \"([^\"]*)\"$")
     public void selectSubcategory(String subcategoryName) {
-
+        commonPage.selectCatalogElement(subcategoryName);
     }
 
     @И("^выбирает подраздел \"([^\"]*)\"$")
     public void selectSubsection(String subsectionName) {
-
+        commonPage.selectCatalogElement(subsectionName);
     }
 
     @И("^устанавливает количество товара \"([^\"]*)\" шт в блоке 'Купить'$")
-    public void setAmountOfProductInBuyBlock(String amount) {
-
+    public void setAmountOfProductInBuyBlock(String amountProduct) {
+        cardProductPage.setAmountOfProductInBuyBlock(amountProduct);
     }
 
     @И("^добавляет товар в корзину из каточки товара$")
     public void addProductToCartFromProductCard () {
-
+        cardProductPage.addProductToCartFromProductCard();
     }
 
     @И("^нажимает кнопку \"([^\"]*)\"$")
     public void clickButton(String buttonName) {
-
+        commonPage.clickLink(buttonName);
     }
 
 
