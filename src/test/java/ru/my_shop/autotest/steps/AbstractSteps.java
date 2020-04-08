a0dfd245270f8985a0a550d97f3f5adbe8480a1c
fkost <f.kostyuchenko@solarl.ru>
2020-04-09 01:01:47 +0300

    Создан логгер в CommonPage

--- src/test/java/ru/my_shop/autotest/steps/AbstractSteps.java
+++ src/test/java/ru/my_shop/autotest/steps/AbstractSteps.java
@@ -1,4 +1,10 @@
 package ru.my_shop.autotest.steps;
 
-public class AbstractSteps {
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public abstract class AbstractSteps {
+
+    protected static final Logger logger
+            = LoggerFactory.getLogger(CommonSteps.class);
 }
