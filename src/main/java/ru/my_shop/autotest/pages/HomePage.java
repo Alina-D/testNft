a4e2fb67c0785d9d3fc15dcf27ea5ce83eb1f8db
fkost <f.kostyuchenko@solarl.ru>
2020-04-20 00:16:25 +0300

    Реализована логика работы с главной страницей

--- src/main/java/ru/my_shop/autotest/pages/HomePage.java
+++ src/main/java/ru/my_shop/autotest/pages/HomePage.java
@@ -1,4 +1,35 @@
 package ru.my_shop.autotest.pages;
 
-public class HomePage {
+import com.codeborne.selenide.ElementsCollection;
+import ru.my_shop.autotest.models.ProductModel;
+
+import static com.codeborne.selenide.Selenide.$$;
+
+/**
+ * Класс описывающий страницу "Главная страница"
+ */
+public class HomePage extends CommonPage{
+
+
+    // Информация о товаре на главной странице
+    private final ElementsCollection productInfoOnHomePage = $$("[data-o='show_case_6']  .hb_text div");
+
+    /**
+     * Получает и сохраняет информацию о товаре на главной странице
+     */
+    public HomePage getAndSaveProductInfoOnHomePage() {
+        // получает информацию о товаре
+        String productName = productInfoOnHomePage.get(0).getText();
+        String manufacturerProducts = productInfoOnHomePage.get(1).getText();
+        String priceProducts = productInfoOnHomePage.get(2).getText();
+
+        // сохраняет информацию о товаре
+        ProductModel product = config.getProductModel();
+        logger.info(product.toString());
+        product.setProductName(productName);
+        product.setPriceProduct(priceProducts);
+        product.getDetailInfoProduct().put("Производитель", manufacturerProducts);
+        logger.info(product.toString());
+        return this;
+    }
 }
