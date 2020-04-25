2c39fb99420c62fc322c38309d1de008610e56cd
fkost <f.kostyuchenko@solarl.ru>
2020-04-26 02:14:14 +0300

    Разделен метод с проверкой товаров корзине

--- src/main/java/ru/my_shop/autotest/pages/CartPage.java
+++ src/main/java/ru/my_shop/autotest/pages/CartPage.java
@@ -14,7 +14,7 @@ import static org.junit.Assert.assertTrue;
 public class CartPage extends CommonPage {
 
     // ------------------------------------------- Конструктор -----------------------------------------------
-    public CartPage () {
+    public CartPage() {
         super();
     }
 
@@ -31,31 +31,80 @@ public class CartPage extends CommonPage {
     // --------------------------------------------- Методы ------------------------------------------------
 
     /**
-     * Проверяет наличие товара в указанном количестве
-     *
-     * @param amount - количество едениц товара
+     * Проверяет наличие товара в корзине (наименование, цену, количество и сумму товара)
+     * todo убрать InAmount
+     * @param amountProduct - количество едениц товара
      * @return this - ссылка на текущий объект
-     *               todo разбить метод на части
      */
-    public CartPage checkAvailabilityOfProductInAmount(int amount) {
-        String productName = config.accessProductModel().getName();
-        String priceProduct = config.accessProductModel().getPrice().split(" руб.")[0];
-        String amountProduct = amount_field.getAttribute("value");
-        int sum = Integer.parseInt(priceProduct) * amount;
-        String priceProductInCart =
-                getElementText($$x(format(TYPE_SORTING_LINK_XPATH, productName)).get(3)).split(".00")[0];
-        String sumInCart = getElementText($$x(format(TYPE_SORTING_LINK_XPATH, productName)).get(5))
-                .split(".00")[0].replace(" ", "");
+    public CartPage checkAvailabilityOfProductInAmount(int amountProduct) {
+        checkNameProduct();
+        checkPriceProduct();
+        checkAmountProduct(amountProduct);
+        checkSumProduct(amountProduct);
+        return this;
+    }
 
+    /**
+     * Проверяет наименование товара в корзине
+     *  todo проверяет наличие, а не соответствие имени
+     * @return this - ссылка на текущий объект
+     */
+    private CartPage checkNameProduct() {
+        String productName = config.getProductModel().getName();
         assertTrue("Нет товара в корзине",
                 $x(format(LINK_WITH_NAME_XPATH, productName)).isDisplayed());
+        logger.info("Товар с с именем '{}' присутствует в корзине", productName);
+        return this;
+    }
+
+    /**
+     * Проверяет цену товара в корзине
+     *
+     * @return this - ссылка на текущий объект
+     */
+    private CartPage checkPriceProduct() {
+        String productName = config.getProductModel().getName();
+
+        // приводим цену к общему виду
+        String priceProduct = config.getProductModel().getPrice().split(" руб.")[0];
+        String priceProductInCart =
+                getElementText($$x(format(TYPE_SORTING_LINK_XPATH, productName)).get(3)).split(".00")[0];
+
         assertEquals("Товар содержит не верную цену за единицу товара",
                 priceProductInCart, priceProduct);
+        logger.info("Товар содержит верную цену '{}' за единицу товара", priceProduct);
+        return this;
+    }
+
+    /**
+     * Проверяет сумму товара в корзине
+     *
+     * @param amountProduct - количество товара
+     * @return this - ссылка на текущий объект
+     */
+    private CartPage checkSumProduct(int amountProduct) {
+        String productName = config.getProductModel().getName();
+        String priceProduct = config.getProductModel().getPrice().split(" руб.")[0];
+        int sum = Integer.parseInt(priceProduct) * amountProduct;
+        String sumInCart = getElementText($$x(format(TYPE_SORTING_LINK_XPATH, productName)).get(5))
+                .split(".00")[0].replace(" ", "");
+        assertEquals("Сумма товара расчитана не верно", sumInCart, Integer.toString(sum));
+        logger.info("Товар содержит верную сумма {}", amountProduct);
+        return this;
+    }
+
+    /**
+     * Проверяет количество товара в корзине
+     *
+     * @param amountProduct - количество товара
+     * @return this - ссылка на текущий объект
+     */
+    private CartPage checkAmountProduct(int amountProduct) {
+        // todo amount_field с большой буквы?
+        String amountProductInCart = amount_field.getAttribute("value");
         assertEquals("У товара установлено не верное количество",
-                amountProduct, Integer.toString(amount));
-        logger.info(Integer.toString(sum));
-        assertEquals("Сумма товара расчитана не верно",
-                sumInCart, Integer.toString(sum));
+                amountProductInCart, Integer.toString(amountProduct));
+        logger.info("Товар содержит верное количество {}", amountProduct);
         return this;
     }
 }
