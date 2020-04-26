6a3eae46dbd1c9984aad25c5efe4e66f1d487335
fkost <f.kostyuchenko@solarl.ru>
2020-04-26 17:35:35 +0300

    Добавлено описание интерфейса GettingProductInfo

--- src/main/java/ru/my_shop/autotest/interfaces/GettingProductInfo.java
+++ src/main/java/ru/my_shop/autotest/interfaces/GettingProductInfo.java
@@ -1,12 +1,37 @@
 package ru.my_shop.autotest.interfaces;
 
-// todo описать класс и методы
+import ru.my_shop.autotest.models.ProductModel;
+
+/**
+ * Интерфейс описывающий получение информации о товаре
+ */
 public interface GettingProductInfo {
 
-    GettingProductInfo setProductInfo(int numberProduct);
+    /**
+     * Установить информацию о товаре
+     *
+     * @param numberProduct - номер товара в списке каталога
+     * @param product - объект описывающий товар
+     * @return this - ссылка на текущий объект
+     */
+    GettingProductInfo setProductInfo(int numberProduct, ProductModel product);
 
-    GettingProductInfo setParameterName(int numberProduct);
+    /**
+     * Установить информацию о товаре
+     *
+     * @param numberProduct - номер товара в списке каталога
+     * @param product - объект описывающий товар
+     * @return this - ссылка на текущий объект
+     */
+    GettingProductInfo setParameterName(int numberProduct, ProductModel product);
 
-    GettingProductInfo setParameterPrice(int numberProduct);
+    /**
+     * Установить параметр 'Цена' товара
+     *
+     * @param numberProduct - номер товара в списке каталога
+     * @param product - объект описывающий товар
+     * @return this - ссылка на текущий объект
+     */
+    GettingProductInfo setParameterPrice(int numberProduct, ProductModel product);
 
 }
